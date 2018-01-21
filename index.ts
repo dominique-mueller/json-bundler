8ac208e1ab5d1c16118e0cc826b19765196526c4
Dominique Müller <dominique.m.mueller@gmail.com>
2018-01-21 18:08:20 +0100

    Add Travis CI configuration, clean npm install

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
 export * from './src/json-bundler';
-
-import { JSONBundler } from './src/json-bundler';
-
-new JSONBundler().bundle( './test/entry.json5', './dist/result.json' );
-new JSONBundler().bundle( './test/entry.json5', './dist/result.min.json', true );
