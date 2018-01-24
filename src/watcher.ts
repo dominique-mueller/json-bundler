b84bf463cb00b453536b21e917ff80befd88cff1
Dominique <dominique.m.mueller@gmail.com>
2018-01-24 23:19:48 +0100

    Add TODO comments

--- src/watcher.ts
+++ src/watcher.ts
@@ -18,6 +18,10 @@ export function watch() {
 		console.log( 'DELETE' );
 		jsonBundler.bundle( './test/two-level-merge-references/src/input.json', './dist/result.json' );
 	} );
+	// TODO: More watcher, e.g. delete dir - does it matter?
+
+	// TODO: Add / remove files to be watched
+	// TODO: Caching of the original files?
 
 }
 
