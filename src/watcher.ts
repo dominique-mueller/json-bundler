01d93a4a681c79ab99aead4648b8c74f26cd8efd
Dominique <dominique.m.mueller@gmail.com>
2018-01-25 22:53:40 +0100

    Enhance watch list modifications

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,3 +1,5 @@
+import * as path from 'path';
+
 import * as chokidar from 'chokidar';
 
 import { JSONBundler } from './json-bundler';
@@ -9,15 +11,37 @@ export function watch() {
 	jsonBundler.bundle( './test/two-level-merge-references/src/input.json', './dist/result.json' );
 	console.log( 'Done.' );
 
-	const watcher: any = chokidar.watch( jsonBundler.paths );
-	watcher.on( 'change', ( path ) => {
-		console.log( 'CHANGE' );
+	// Watch files
+	let watchedFiles: Array<string> = jsonBundler.paths;
+	const watcher: any = chokidar.watch( watchedFiles );
+	console.log( `Watching ${ watchedFiles.length } files now. Waiting for changes ...` );
+
+	// React on file changes
+	watcher.on( 'change', ( changedPath ) => {
+
+		console.log( `-> File "${ path.basename( changedPath ) }" has changed. Building ...` );
 		jsonBundler.bundle( './test/two-level-merge-references/src/input.json', './dist/result.json' );
+		const newPaths: Array<string> = jsonBundler.paths;
+
+		const added = newPaths.filter( currentPath => !watchedFiles.includes( currentPath ) );
+		watcher.add( added );
+		if ( added.length > 0 ) {
+			console.log( `   Added ${ added.length } files to the watcher.` )
+		}
+
+		const removed = watchedFiles.filter( currentPath => !newPaths.includes( currentPath ) );
+		watcher.unwatch( removed );
+		if ( removed.length > 0 ) {
+			console.log( `   Removed ${ removed.length } files from the watcher.` )
+		}
+
+		watchedFiles = newPaths;
+
 	} );
-	watcher.on( 'unlink', ( path ) => {
-		console.log( 'DELETE' );
-		jsonBundler.bundle( './test/two-level-merge-references/src/input.json', './dist/result.json' );
-	} );
+	// watcher.on( 'unlink', ( path ) => {
+	// 	console.log( 'DELETE' );
+	// 	jsonBundler.bundle( './test/two-level-merge-references/src/input.json', './dist/result.json' );
+	// } );
 	// TODO: More watcher, e.g. delete dir - does it matter?
 
 	// TODO: Add / remove files to be watched
