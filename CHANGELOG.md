46bf31fbfeac906b98900ef08bef829b48d092ab
Dominique Müller <dominique.m.mueller@gmail.com>
2021-02-08 02:40:37 +0100

    chore(*): Maintanance (#78)
    - Cleanup and simplify README, CHANGELOG
    - Upgrade dependencies, cleanup package.json
    - Migrate to GitHub actions, removed other automation
    - Add linting and formatting
    - Use standardized gitignore

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,44 +1,9 @@
 # Changelog
 
-Also see the **[release page](https://github.com/dominique-mueller/json-bundler/releases)**.
+Also see **[GitHub releases](https://github.com/dominique-mueller/json-bundler/releases)**.
 
 <br>
 
 ## [1.0.0](https://github.com/dominique-mueller/json-bundler/releases/tag/1.0.0) (2018-02-11)
 
-### Chores
-
-* Add pullapprove config ([#14](https://github.com/dominique-mueller/json-bundler/issues/14)) ([4dc0a75](https://github.com/dominique-mueller/json-bundler/commit/4dc0a75))
-* **ci:** Configure Travis CI ([#8](https://github.com/dominique-mueller/json-bundler/issues/8)) ([30f7000](https://github.com/dominique-mueller/json-bundler/commit/30f7000))
-* **ci:** Enable greenkeeper ([#10](https://github.com/dominique-mueller/json-bundler/issues/10)) ([3082625](https://github.com/dominique-mueller/json-bundler/commit/3082625))
-* **codecov:** Add codecov configuration ([#22](https://github.com/dominique-mueller/json-bundler/issues/22)) ([81cd899](https://github.com/dominique-mueller/json-bundler/commit/81cd899))
-* **dependencies:** Add Renovate ([#16](https://github.com/dominique-mueller/json-bundler/issues/16)) ([6b77aa3](https://github.com/dominique-mueller/json-bundler/commit/6b77aa3))
-* **dependencies:** Update dependency [@types](https://github.com/types)/node to 9.4.x ([#17](https://github.com/dominique-mueller/json-bundler/issues/17)) ([a5e8fe1](https://github.com/dominique-mueller/json-bundler/commit/a5e8fe1))
-* **dependencies:** Update dependency jest to 22.2.x ([#21](https://github.com/dominique-mueller/json-bundler/issues/21)) ([bb2900a](https://github.com/dominique-mueller/json-bundler/commit/bb2900a))
-* **dependencies:** Update dependency typescript to 2.7.x ([#20](https://github.com/dominique-mueller/json-bundler/issues/20)) ([df2f4cf](https://github.com/dominique-mueller/json-bundler/commit/df2f4cf))
-
-### Documentation
-
-* **README:** Write README, add preview image, update package.json ([#19](https://github.com/dominique-mueller/json-bundler/issues/19)) ([a3a0599](https://github.com/dominique-mueller/json-bundler/commit/a3a0599))
-
-### Features
-
-* **bin:** Add logging ([#15](https://github.com/dominique-mueller/json-bundler/issues/15)) ([5a3a65f](https://github.com/dominique-mueller/json-bundler/commit/5a3a65f))
-* **bin:** Add watch mode ([#13](https://github.com/dominique-mueller/json-bundler/issues/13)) ([5b818ef](https://github.com/dominique-mueller/json-bundler/commit/5b818ef))
-* **cli:** Add CLI binary ([#11](https://github.com/dominique-mueller/json-bundler/issues/11)) ([dc793df](https://github.com/dominique-mueller/json-bundler/commit/dc793df))
-* **errors:** Implement error handling for reading & writing files ([#5](https://github.com/dominique-mueller/json-bundler/issues/5)) ([0c17c45](https://github.com/dominique-mueller/json-bundler/commit/0c17c45))
-* **json-bundler:** Add json-bundler class w/ interfaces ([#1](https://github.com/dominique-mueller/json-bundler/issues/1)) ([d60e20e](https://github.com/dominique-mueller/json-bundler/commit/d60e20e))
-* **output:** Add output minification, configurable ([#6](https://github.com/dominique-mueller/json-bundler/issues/6)) ([74861ca](https://github.com/dominique-mueller/json-bundler/commit/74861ca))
-* **read-file:** Add JSON5 support, remove auto file-extension ([#4](https://github.com/dominique-mueller/json-bundler/issues/4)) ([5b875c2](https://github.com/dominique-mueller/json-bundler/commit/5b875c2))
-* **references:** Add missing .json file extension to paths if missing ([#2](https://github.com/dominique-mueller/json-bundler/issues/2)) ([7b17fb0](https://github.com/dominique-mueller/json-bundler/commit/7b17fb0))
-* **references:** Resolve node_modules paths, fix merging ([#3](https://github.com/dominique-mueller/json-bundler/issues/3)) ([6e10f65](https://github.com/dominique-mueller/json-bundler/commit/6e10f65))
-
-### Tests
-
-* Add tests ([#7](https://github.com/dominique-mueller/json-bundler/issues/7)) ([2e405ed](https://github.com/dominique-mueller/json-bundler/commit/2e405ed))
-
-<br>
-
----
-
-<sup>*Changelog generated automatically by [automatic-release](https://github.com/dominique-mueller/automatic-release).*</sup>
+**Initial release**
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -6,256 +6,228 @@ import { JSONBundler } from './../src/json-bundler';
 /**
  * Test Suite
  */
-describe( 'JSON bundler', () => {
-
-    it( 'should instantiate', () => {
-
-        const jsonBundler: JSONBundler = new JSONBundler();
-
-        expect( jsonBundler ).toBeDefined();
-
-    } );
-
-    describe( '(no references)', () => {
-
-        it( 'should bundle the JSON file', () => {
-            test( 'no-references', 'json', false, [], [] );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'no-references', 'json5', false, [], [] );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'no-references', 'json', true, [], [] );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'no-references', 'json5', true, [], [] );
-        } );
-
-    } );
-
-    describe( '(one-level references)', () => {
-
-        const paths: Array<string> = [
-            './shared/copyright'
-        ];
-
-        it( 'should bundle the JSON file', () => {
-            test( 'one-level-references', 'json', false, paths, [] );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'one-level-references', 'json5', false, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'one-level-references', 'json', true, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'one-level-references', 'json5', true, paths, [] );
-        } );
-
-    } );
-
-    describe( '(one-level references and merging)', () => {
-
-        const paths: Array<string> = [
-            './shared/copyright'
-        ];
-
-        it( 'should bundle the JSON file', () => {
-            test( 'one-level-merge-references', 'json', false, paths, [] );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'one-level-merge-references', 'json5', false, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'one-level-merge-references', 'json', true, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'one-level-merge-references', 'json5', true, paths, [] );
-        } );
-
-    } );
-
-    describe( '(two-level references)', () => {
-
-        const paths: Array<string> = [
-            './shared/footer',
-            './shared/copyright'
-        ];
-
-        it( 'should bundle the JSON file', () => {
-            test( 'two-level-references', 'json', false, paths, [] );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'two-level-references', 'json5', false, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'two-level-references', 'json', true, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'two-level-references', 'json5', true, paths, [] );
-        } );
-
-    } );
-
-    describe( '(two-level references and merging)', () => {
-
-        const paths: Array<string> = [
-            './shared/footer',
-            './shared/copyright'
-        ];
-
-        it( 'should bundle the JSON file', () => {
-            test( 'two-level-merge-references', 'json', false, paths, [] );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'two-level-merge-references', 'json5', false, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'two-level-merge-references', 'json', true, paths, [] );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'two-level-merge-references', 'json5', true, paths, [] );
-        } );
-
-    } );
-
-    describe( '(node_modules references)', () => {
-
-        const externalPaths: Array<string> = [
-            './json-bundler-test/copyright'
-        ];
-
-        it( 'should bundle the JSON file', () => {
-            test( 'node-module-references', 'json', false, [], externalPaths );
-        } );
-
-        it( 'should bundle the JSON5 file', () => {
-            test( 'node-module-references', 'json5', false, [], externalPaths );
-        } );
-
-        it( 'should bundle and minify the JSON file', () => {
-            test( 'node-module-references', 'json', true, [], externalPaths );
-        } );
-
-        it( 'should bundle and minify the JSON5 file', () => {
-            test( 'node-module-references', 'json5', true, [], externalPaths );
-        } );
-
-    } );
-
-    describe( '(errors)', () => {
-
-        it( 'should throw an error if a referenced file is not JSON / JSON5', () => {
-
-            let error: any = null;
-            try {
-                new JSONBundler().bundle( './test/errors/src/input-invalid-file-type.json', './test/errors/dist/output.json' );
-            } catch ( jsonBundlerError ) {
-                error = jsonBundlerError;
-            }
-
-            expect( error ).not.toBeNull();
-            expect( error.message.split( '\n' )[ 0 ] ).toBe( `The file "${ path.resolve( './test/errors/src/does-not-exist.txt' ) }" is not of type JSON / JSON5.` );
-            expect( error.message.split( '\n' )[ 1 ] ).toBe( `Details: File is referenced in "${ path.resolve( './test/errors/src/input-invalid-file-type.json' ) }".` );
-
-        } );
-
-        it( 'should throw an error if a referenced file does not exist', () => {
-
-            let error: any = null;
-            try {
-                new JSONBundler().bundle( './test/errors/src/input-reference-does-not-exist.json', './test/errors/dist/output.json' );
-            } catch ( jsonBundlerError ) {
-                error = jsonBundlerError;
-            }
-
-            expect( error ).not.toBeNull();
-            expect( error.message.split( '\n' )[ 0 ] ).toContain( `An error occured while reading the file "${ path.resolve( './test/errors/src/does-not-exist.json' ) }".` );
-            expect( error.message.split( '\n' )[ 1 ] ).toBe( `Details: File is referenced in "${ path.resolve( './test/errors/src/input-reference-does-not-exist.json' ) }".` );
-
-        } );
-
-        it( 'should throw an error if a referenced file is invalid JSON', () => {
-
-            let error: any = null;
-            try {
-                new JSONBundler().bundle( './test/errors/src/input-invalid-syntax.json', './test/errors/dist/output.json' );
-            } catch ( jsonBundlerError ) {
-                error = jsonBundlerError;
-            }
-
-            expect( error ).not.toBeNull();
-            expect( error.message.split( '\n' )[ 0 ] ).toContain( `An error occured while parsing the file "${ path.resolve( './test/errors/src/shared/invalid.json' ) }" as JSON.` );
-            expect( error.message.split( '\n' )[ 1 ] ).toBe( `Details: File is referenced in "${ path.resolve( './test/errors/src/input-invalid-syntax.json' ) }".` );
-
-        } );
-
-        it( 'should throw an error if a referenced file is invalid JSON5', () => {
-
-            let error: any = null;
-            try {
-                new JSONBundler().bundle( './test/errors/src/input-invalid-syntax.json5', './test/errors/dist/output.json' );
-            } catch ( jsonBundlerError ) {
-                error = jsonBundlerError;
-            }
-
-            expect( error ).not.toBeNull();
-            expect( error.message.split( '\n' )[ 0 ] ).toContain( `An error occured while parsing the file "${ path.resolve( './test/errors/src/shared/invalid.json5' ) }" as JSON5.` );
-            expect( error.message.split( '\n' )[ 1 ] ).toBe( `Details: File is referenced in "${ path.resolve( './test/errors/src/input-invalid-syntax.json5' ) }".` );
-
-        } );
-
-        it( 'should throw an error if writing the bundle fails', () => {
-
-            let error: any = null;
-            try {
-                new JSONBundler().bundle( './test/no-references/src/input.json', '' ); // Empty path
-            } catch ( jsonBundlerError ) {
-                error = jsonBundlerError;
-            }
-
-            expect( error ).not.toBeNull();
-            expect( error.message ).toContain( `An error occured while writing the file "${ process.cwd() }".` );
-
-        } );
-
-    } );
-
-} );
-
-function test( folderName: string, jsonType: 'json' | 'json5', minified: boolean, paths: Array<string>, externalPaths: Array<string> ): void {
-
+describe('JSON bundler', () => {
+  it('should instantiate', () => {
     const jsonBundler: JSONBundler = new JSONBundler();
-    jsonBundler.bundle( `./test/${ folderName }/src/input.${ jsonType }`, `./test/${ folderName }/dist/output.json`, minified );
 
-    const output: any = JSON.parse( fs.readFileSync( `./test/${ folderName }/dist/output.json`, 'utf-8' ) );
-    const expectedOutput: any = JSON.parse( fs.readFileSync( `./test/${ folderName }/src/expected-output.json`, 'utf-8' ) );
+    expect(jsonBundler).toBeDefined();
+  });
 
-    const expectedPaths: Array<string> = [ 'input', ...paths ]
-        .map( ( filePath: string ): string => {
-            return path.resolve( 'test', folderName, 'src', `${ filePath }.${ jsonType }` );
-        } );
-    const expectedExternalPaths: Array<string> = externalPaths
-        .map( ( externalFilePath: string ): string => {
-            return path.resolve( process.cwd(), 'node_modules', `${ externalFilePath }.${ jsonType }` );
-        } );
+  describe('(no references)', () => {
+    it('should bundle the JSON file', () => {
+      test('no-references', 'json', false, [], []);
+    });
 
-    expect( output ).toEqual( expectedOutput );
-    expect( jsonBundler.paths.sort() ).toEqual( [ ...expectedPaths, ...expectedExternalPaths ].sort() ); // Ignore order
+    it('should bundle the JSON5 file', () => {
+      test('no-references', 'json5', false, [], []);
+    });
 
+    it('should bundle and minify the JSON file', () => {
+      test('no-references', 'json', true, [], []);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('no-references', 'json5', true, [], []);
+    });
+  });
+
+  describe('(one-level references)', () => {
+    const paths: Array<string> = ['./shared/copyright'];
+
+    it('should bundle the JSON file', () => {
+      test('one-level-references', 'json', false, paths, []);
+    });
+
+    it('should bundle the JSON5 file', () => {
+      test('one-level-references', 'json5', false, paths, []);
+    });
+
+    it('should bundle and minify the JSON file', () => {
+      test('one-level-references', 'json', true, paths, []);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('one-level-references', 'json5', true, paths, []);
+    });
+  });
+
+  describe('(one-level references and merging)', () => {
+    const paths: Array<string> = ['./shared/copyright'];
+
+    it('should bundle the JSON file', () => {
+      test('one-level-merge-references', 'json', false, paths, []);
+    });
+
+    it('should bundle the JSON5 file', () => {
+      test('one-level-merge-references', 'json5', false, paths, []);
+    });
+
+    it('should bundle and minify the JSON file', () => {
+      test('one-level-merge-references', 'json', true, paths, []);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('one-level-merge-references', 'json5', true, paths, []);
+    });
+  });
+
+  describe('(two-level references)', () => {
+    const paths: Array<string> = ['./shared/footer', './shared/copyright'];
+
+    it('should bundle the JSON file', () => {
+      test('two-level-references', 'json', false, paths, []);
+    });
+
+    it('should bundle the JSON5 file', () => {
+      test('two-level-references', 'json5', false, paths, []);
+    });
+
+    it('should bundle and minify the JSON file', () => {
+      test('two-level-references', 'json', true, paths, []);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('two-level-references', 'json5', true, paths, []);
+    });
+  });
+
+  describe('(two-level references and merging)', () => {
+    const paths: Array<string> = ['./shared/footer', './shared/copyright'];
+
+    it('should bundle the JSON file', () => {
+      test('two-level-merge-references', 'json', false, paths, []);
+    });
+
+    it('should bundle the JSON5 file', () => {
+      test('two-level-merge-references', 'json5', false, paths, []);
+    });
+
+    it('should bundle and minify the JSON file', () => {
+      test('two-level-merge-references', 'json', true, paths, []);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('two-level-merge-references', 'json5', true, paths, []);
+    });
+  });
+
+  describe('(node_modules references)', () => {
+    const externalPaths: Array<string> = ['./json-bundler-test/copyright'];
+
+    it('should bundle the JSON file', () => {
+      test('node-module-references', 'json', false, [], externalPaths);
+    });
+
+    it('should bundle the JSON5 file', () => {
+      test('node-module-references', 'json5', false, [], externalPaths);
+    });
+
+    it('should bundle and minify the JSON file', () => {
+      test('node-module-references', 'json', true, [], externalPaths);
+    });
+
+    it('should bundle and minify the JSON5 file', () => {
+      test('node-module-references', 'json5', true, [], externalPaths);
+    });
+  });
+
+  describe('(errors)', () => {
+    it('should throw an error if a referenced file is not JSON / JSON5', () => {
+      let error: any = null;
+      try {
+        new JSONBundler().bundle('./test/errors/src/input-invalid-file-type.json', './test/errors/dist/output.json');
+      } catch (jsonBundlerError) {
+        error = jsonBundlerError;
+      }
+
+      expect(error).not.toBeNull();
+      expect(error.message.split('\n')[0]).toBe(
+        `The file "${path.resolve('./test/errors/src/does-not-exist.txt')}" is not of type JSON / JSON5.`,
+      );
+      expect(error.message.split('\n')[1]).toBe(
+        `Details: File is referenced in "${path.resolve('./test/errors/src/input-invalid-file-type.json')}".`,
+      );
+    });
+
+    it('should throw an error if a referenced file does not exist', () => {
+      let error: any = null;
+      try {
+        new JSONBundler().bundle('./test/errors/src/input-reference-does-not-exist.json', './test/errors/dist/output.json');
+      } catch (jsonBundlerError) {
+        error = jsonBundlerError;
+      }
+
+      expect(error).not.toBeNull();
+      expect(error.message.split('\n')[0]).toContain(
+        `An error occured while reading the file "${path.resolve('./test/errors/src/does-not-exist.json')}".`,
+      );
+      expect(error.message.split('\n')[1]).toBe(
+        `Details: File is referenced in "${path.resolve('./test/errors/src/input-reference-does-not-exist.json')}".`,
+      );
+    });
+
+    it('should throw an error if a referenced file is invalid JSON', () => {
+      let error: any = null;
+      try {
+        new JSONBundler().bundle('./test/errors/src/input-invalid-syntax.json', './test/errors/dist/output.json');
+      } catch (jsonBundlerError) {
+        error = jsonBundlerError;
+      }
+
+      expect(error).not.toBeNull();
+      expect(error.message.split('\n')[0]).toContain(
+        `An error occured while parsing the file "${path.resolve('./test/errors/src/shared/invalid.json')}" as JSON.`,
+      );
+      expect(error.message.split('\n')[1]).toBe(
+        `Details: File is referenced in "${path.resolve('./test/errors/src/input-invalid-syntax.json')}".`,
+      );
+    });
+
+    it('should throw an error if a referenced file is invalid JSON5', () => {
+      let error: any = null;
+      try {
+        new JSONBundler().bundle('./test/errors/src/input-invalid-syntax.json5', './test/errors/dist/output.json');
+      } catch (jsonBundlerError) {
+        error = jsonBundlerError;
+      }
+
+      expect(error).not.toBeNull();
+      expect(error.message.split('\n')[0]).toContain(
+        `An error occured while parsing the file "${path.resolve('./test/errors/src/shared/invalid.json5')}" as JSON5.`,
+      );
+      expect(error.message.split('\n')[1]).toBe(
+        `Details: File is referenced in "${path.resolve('./test/errors/src/input-invalid-syntax.json5')}".`,
+      );
+    });
+
+    it('should throw an error if writing the bundle fails', () => {
+      let error: any = null;
+      try {
+        new JSONBundler().bundle('./test/no-references/src/input.json', ''); // Empty path
+      } catch (jsonBundlerError) {
+        error = jsonBundlerError;
+      }
+
+      expect(error).not.toBeNull();
+      expect(error.message).toContain(`An error occured while writing the file "${process.cwd()}".`);
+    });
+  });
+});
+
+function test(folderName: string, jsonType: 'json' | 'json5', minified: boolean, paths: Array<string>, externalPaths: Array<string>): void {
+  const jsonBundler: JSONBundler = new JSONBundler();
+  jsonBundler.bundle(`./test/${folderName}/src/input.${jsonType}`, `./test/${folderName}/dist/output.json`, minified);
+
+  const output: any = JSON.parse(fs.readFileSync(`./test/${folderName}/dist/output.json`, 'utf-8'));
+  const expectedOutput: any = JSON.parse(fs.readFileSync(`./test/${folderName}/src/expected-output.json`, 'utf-8'));
+
+  const expectedPaths: Array<string> = ['input', ...paths].map((filePath: string): string => {
+    return path.resolve('test', folderName, 'src', `${filePath}.${jsonType}`);
+  });
+  const expectedExternalPaths: Array<string> = externalPaths.map((externalFilePath: string): string => {
+    return path.resolve(process.cwd(), 'node_modules', `${externalFilePath}.${jsonType}`);
+  });
+
+  expect(output).toEqual(expectedOutput);
+  expect(jsonBundler.paths.sort()).toEqual([...expectedPaths, ...expectedExternalPaths].sort()); // Ignore order
 }
